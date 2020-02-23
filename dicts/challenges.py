169e9f8700985410978c539a90e58ee27d6d3095
Timur Safin <timur@getgain.ru>
2020-02-23 14:02:23 +0300

    Update

--- dicts/challenges.py
+++ dicts/challenges.py
@@ -18,4 +18,46 @@ def build_query_string(query_string):
         res.append(f'{param}={val}')
     return '&'.join(res)
 
-print(build_query_string({'per': 10, 'page': 1}))
+
+NUMERALS = {  # noqa: WPS407
+    'M': 1000,
+    'CM': 900,
+    'D': 500,
+    'CD': 400,
+    'C': 100,
+    'XC': 90,
+    'L': 50,
+    'XL': 40,
+    'X': 10,
+    'IX': 9,
+    'V': 5,
+    'IV': 4,
+    'I': 1,
+}
+
+
+def to_roman(number):
+    roman = ''
+    for symbol, value in NUMERALS.items():
+        while number >= value:
+            roman += symbol
+            number -= value
+        if number <= 0:
+            break
+    return roman
+
+
+def to_arabic(roman):
+    arabic = 0
+    temp = roman
+    while len(roman):
+        if NUMERALS.get(roman):
+            arabic += NUMERALS[roman]
+            break
+
+        while not NUMERALS.get(temp):
+            temp = temp[:-1]
+
+        arabic += NUMERALS[temp]
+        temp = roman = roman[len(temp):]
+    return arabic
